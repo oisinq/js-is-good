53bfee3dbada26e6ddbced8216e971a589a7cae5
Oisín Quinn <oisin1001@gmail.com>
2021-12-24 21:15:44 +0000

    Create pls_ignore_this.js

---
+++ js/pls_ignore_this.js
@@ -0,0 +1,11 @@
+/* Please ignore this file!
+
+I realised that this project has more TypeScript than Javascript
+
+So I made this file with random code in it! 
+
+But you probably shouldn't run it. */
+
+while (true) {
+  console.log("woooooooooooooo);
+}
